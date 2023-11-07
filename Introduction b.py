4b9b31b50e35730f7d8256d0f27c2060b36ffd75
tukuchrispus <tukuchrispus@gmail.com>
2023-11-07 13:02:13 +0300

    changes

---
+++ Introduction b.py
@@ -0,0 +1,38 @@
+# Dataypes in Python
+
+# numerals
+num=24 #integer (int)
+num2= "24" # string (str)
+pie=3.14 # float
+print(num,num2,pie)
+# end of Numerals
+# double -3.1424628472492
+# string
+wish = "happy birthday uhuru kenyatta"
+age = "19"
+
+# End of string
+# collection of items ...array
+# list,tuple,range
+# list
+crushes =["mariam","winnie","murife","embarambamba",254] [1,2,3]]
+ # square brackets
+# Accessing item in the list
+print(crushes[1])
+print("Hi", crushes[1],"will you come over?")
+print(crushes)
+# mutable-can alter
+# print(crushes)
+#tuple
+()# parenthesis
+mombasa= ("beach boys","wababaz",254)
+print("leo nataka",mombasa[0])
+
+# end of arrays
+# Dictionary
+countries = {
+"Africa":"Kenya"pwani, "SA"},
+"europe":"German",}
+print(countries)
+
+
